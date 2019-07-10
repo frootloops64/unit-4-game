58f74053fca16afa1b979bdaad02a3f08c1dcff7
Jeremy Chiu <jeremybarotchiu@gmail.com>
2019-07-10 16:20:32 +1000

    added structure to html file and added vars for js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+    var magicNumber;
+    var totalScore = 0;
+    var wins = 0;
+    var losses = 0;
+    var crystal1Num;
+    var crystal2Num;
+    var crystal3Num;
+    var crystal4Num;
+
+    function newNumbers() {
+        magicNumber = Math.floor(Math.random() * 102) + 19;
+        crystal1Num = Math.floor(Math.random() * 12) + 1;
+        crystal2Num = Math.floor(Math.random() * 12) + 1;
+        crystal3Num = Math.floor(Math.random() * 12) + 1;
+        crystal4Num = Math.floor(Math.random() * 12) + 1;
+       
+    }
+
+    newNumbers();
+    console.log(magicNumber);
+    console.log(crystal1Num);
