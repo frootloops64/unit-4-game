4d2ebce39668fe37bfe481ac132cfde45afcf471
Jeremy Chiu <jeremybarotchiu@gmail.com>
2019-07-11 16:21:44 +1000

    testing of game shown NaN error. not sure how to solve

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+$(document).ready(function () {
+
     var targetNumber;
     var totalScore = 0;
     var wins = 0;
@@ -13,15 +15,64 @@
         crystal2Num = Math.floor(Math.random() * 12) + 1;
         crystal3Num = Math.floor(Math.random() * 12) + 1;
         crystal4Num = Math.floor(Math.random() * 12) + 1;
-       
+
     }
+    newNumbers();
+    console.log(targetNumber);
+    console.log(crystal1Num);
 
     function newGame() {
         newNumbers();
         totalScore = 0;
-        $
+        $("#targetNumber").text(targetNumber);
+        $("#totalScore").text(totalScore);
+        $("#crystal1").attr("crystalValue", crystal1Num);
+        $("#crystal2").attr("crystalValue", crystal2Num);
+        $("#crystal3").attr("crystalValue", crystal3Num);
+        $("#crystal4").attr("crystalValue", crystal4Num);
+        $("#wins").text(wins);
+        $("#losses").text(losses);
+
+        console.log(crystal1Num);
     }
 
-    newNumbers();
-    console.log(magicNumber);
-    console.log(crystal1Num);
+    function youWin() {
+        $("#winOrLose").text("You Win!")
+        wins++;
+        $("#wins").text(wins);
+    }
+
+    function youLose() {
+        $("#winOrLose").text("You Lose!")
+        losses++;
+        $("#losses").text(losses);
+    }
+
+    newGame();
+
+    $(".crystalimage").on("click", function () {
+        if (totalScore >= targetNumber) {
+            return;
+        }
+        console.log(crystalValue);
+        var crystalValue = $(".crystalimage").attr("crystalValue");
+        crystalValue = parseInt(crystalValue);
+        totalScore += crystalValue;
+        $("#totalScore").text(totalScore);
+
+        console.log(totalScore);
+        console.log(crystalValue);
+
+        if (totalScore === targetNumber) {
+            youWin();
+        } else if (totalScore > targetNumber) {
+            youLose();
+        }
+
+    });
+
+    $(".btn").on("click", function () {
+        newGame();
+    });
+
+});
